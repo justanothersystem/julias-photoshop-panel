7b407b885c0ce1b1a7a6598bc16c532faba8c312
Jeffrey Alan Scudder <me@jas.life>
2017-10-08 17:14:22 -0700

    basic functionality in place

--- jp.justanothersystem.org/assets/js/panel.js
+++ jp.justanothersystem.org/assets/js/panel.js
@@ -1,5 +1,7 @@
 import '../sass/main.scss'
 
+// Setting up Amazon Web Services
+
 const bucketName = 'julias-panel-east.justanothersystem.org'
 const bucketRegion = 'us-east-1'
 const identityPoolId = 'us-east-1:4c130e96-cf5d-4eff-898d-283c119b14f6'
@@ -16,39 +18,75 @@ const s3 = new AWS.S3({
   params: {Bucket: bucketName}
 })
 
-s3.listObjects({
-  Bucket: bucketName,
-  MaxKeys: 10
-}, (err, data) => {
-  if (err) {
-    console.error(err)
-  } else {
-    console.log(data)
+let listTimeout
 
-    const images = []
+function list () {
+  console.log('Searching for images...')
+  clearTimeout(listTimeout)
+  s3.listObjects({
+    Bucket: bucketName,
+    MaxKeys: 1
+  }, (err, data) => {
+    if (err) {
+      console.error(err)
+    } else {
+      console.log(data)
 
-    data.Contents.forEach((d) => {
-      images.push(d.Key)
-    })
+      if (data.Contents.length > 0) {
+        const key = data.Contents[0].Key
+        const url = `https://justanothersystem.imgix.net/${key}?w=2048`
+        addImage(url, key)
+        console.log('Image found!', key)
+      } else {
+        console.log('No images found :(')
+        listTimeout = setTimeout(() => {
+          list()
+        }, 10000)
+      }
+    }
+  })
+}
 
-    const urls = images.map((key) => {
-      // return `https://s3.amazonaws.com/${bucketName}/${key}`
-      return `https://justanothersystem.imgix.net/${key}?w=128`
-    })
+list()
 
-    console.log(urls)
+function removeImage (el) {
+  el.classList.add('active')
 
-    urls.forEach(addImage)
+  s3.deleteObject({
+    Bucket: bucketName,
+    Key: el.dataset.key
+  }, function (err, data) {
+    if (err) {
+      console.log(err, err.stack)
+      e.target.classList.remove('active')
+      // TODO: Show the user that the deletion failed.
+    } else {
+      console.log(data)
+      // Remove the image from the page.
+      el.remove()
+      console.log('Removed image.')
+      window.onkeydown = undefined
+      list()
+    }
+  })
+}
 
-  }
-})
-
-function addImage (url) {
+function addImage (url, key) {
   const img = new window.Image()
   img.src = url
-  document.getElementById('images').appendChild(img)
+  img.id = 'image'
+  img.dataset.key = key
+  document.body.appendChild(img)
+  window.onkeydown = function (e) {
+    if (e.key === 'Delete' || e.key === 'Backspace') { removeImage(img) }
+  }
 }
 
-document.getElementById('refresh').onclick = () => {
-  window.location.reload()
+window.onfocus = function (e) {
+  document.body.classList.remove('inactive')
 }
+
+window.onblur = function (e) {
+  document.body.classList.add('inactive')
+}
+
